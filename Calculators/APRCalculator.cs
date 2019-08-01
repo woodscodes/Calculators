23e5d13d55b213b700245b5859dc72c42a9112ea
stuwo <stooberto@gmail.com>
2019-08-01 18:16:26 +0900

    Add project files.

---
+++ Calculators/APRCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators
+{
+    public class APRCalculator
+    {
+        public double GetMonthlyPayment()
+        {
+            int principal = 15200;
+            decimal annualInterestRate = 5 / 100;
+            decimal monthlyInterestRate = annualInterestRate / 12;
+            int termInMonths = 60;
+
+            return Math.Pow(principal * (1 + (double)monthlyInterestRate), termInMonths) / Math.Pow(1 + (double)monthlyInterestRate, termInMonths) - 1;
+
+        }
+
+
+    }
+}
---
+++ Calculators/CompoundCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculators
+{
+    public class CompoundCalculator
+    {
+        public decimal CompoundedInterest()
+        {
+            int principal = 5000;
+            double interestRate = 0.05;
+            int intervalsOfCompounding = 12;
+            int totalPeriod = 10;
+
+            double amount = principal * Math.Pow((1 + interestRate / intervalsOfCompounding), intervalsOfCompounding * totalPeriod);
+            return Math.Round((decimal)amount, 2);
+        }
+    }
+}
