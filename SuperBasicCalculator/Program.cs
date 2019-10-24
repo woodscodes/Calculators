6cf8561d58e9996efb91c715243db90cd9c8df5b
stuwo <stooberto@gmail.com>
2019-10-24 12:48:22 +0900

    Calculator, starting basic getting more advanced as further functionality gets added

---
+++ SuperBasicCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Basic calculator does some super basic arithmetic
+            var n1 = 5;
+            var n2 = 5;
+
+            Add(n1, n2);
+            Subtract(n1, n2);
+            Multiply(n1, n2);
+            Divide(n1, n2);
+
+        }
+
+        private static void Divide(int n1, int n2)
+        {
+            Console.WriteLine(n1 / n2);
+        }
+
+        private static void Multiply(int n1, int n2)
+        {
+            Console.WriteLine(n1 * n2);
+        }
+
+        private static void Subtract(int n1, int n2)
+        {
+            Console.WriteLine(n1 - n2);
+        }
+
+        private static void Add(int n1, int n2)
+        {
+            Console.WriteLine(n1 + n2);
+        }
+    }
+}
--- SuperBasicCalculator/Properties/AssemblyInfo.cs
+++ SuperBasicCalculator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Calculators")]
+[assembly: AssemblyTitle("SuperBasicCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Calculators")]
+[assembly: AssemblyProduct("SuperBasicCalculator")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38bb5c71-4743-4922-8243-3a214b76e184")]
+[assembly: Guid("f5442677-db9c-4145-9dc7-92ee57bb874c")]
 
 // Version information for an assembly consists of the following four values:
 //
